5428e0f448c08a9c80a33a1fb439b4a2f789b02d
Sanchi Varma <emailsanchi@gmail.com>
2025-12-08 20:34:07 +0100

    Updated index.html

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,7 +1,7 @@
 export function Logo({ className = "w-8 h-8" }: { className?: string }) {
   return (
     <img
-      src="/urlstash_trimmed.png"
+      src="/urlstash_trimmed copy.png"
       alt="UrlStash Logo"
       className={className}
     />
